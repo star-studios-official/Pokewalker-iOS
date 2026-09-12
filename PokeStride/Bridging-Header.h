7eb74f8acffa8669cfc5459fcedb07e2229e61ae
Nathaniel Dreemurr <32936782-ndreemurr@users.noreply.gitlab.com>
2026-09-12 14:54:05 -0400

    Fix CI

---
+++ PokeStride/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  PokeStride
+//
+//  Exposes the H8/300H C emulator core to Swift.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#include "Emulator/h8300h.h"
+
+#endif /* Bridging_Header_h */
